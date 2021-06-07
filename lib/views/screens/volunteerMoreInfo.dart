53e937c5ebe251f3304f79a30f44c05688601804
Rohith Kumar Gattu <57759452+Rohithkumargattu979@users.noreply.github.com>
2021-06-07 18:17:34 +0530

    volunteerMoreInfo page done

--- lib/views/screens/volunteerMoreInfo.dart
+++ lib/views/screens/volunteerMoreInfo.dart
@@ -11,6 +11,127 @@ class VolunteerMoreInfoScreen extends StatefulWidget {
 class _VolunteerMoreInfoScreenState extends State<VolunteerMoreInfoScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Center(
+              child: Container(
+                height: 300,
+                width: 300,
+                child: Image(
+                  image: AssetImage('assets/images/NSS-symbol.png'),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Text(
+              'NOT ME BUT YOU!',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Container(
+              width: 350.0,
+              height: 80.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                border: Border.all(color: Colors.black),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Row(
+                  children: [
+                    Text(
+                      'Drop Suggestions',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+                    ),
+                    SizedBox(
+                      width: 60,
+                    ),
+                    Text(
+                      '>',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Container(
+              width: 350.0,
+              height: 80.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                border: Border.all(color: Colors.black),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Row(
+                  children: [
+                    Text(
+                      'Contact Us',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+                    ),
+                    SizedBox(
+                      width: 140,
+                    ),
+                    Text(
+                      '>',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Container(
+              width: 350.0,
+              height: 80.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                border: Border.all(color: Colors.black),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Row(
+                  children: [
+                    Text(
+                      'About Us',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+                    ),
+                    SizedBox(
+                      width: 160,
+                    ),
+                    Text(
+                      '>',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
