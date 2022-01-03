111fbccd4bcf30eb9d049cf29112acf9a5b94308
Sanket-09 <f20200356@hyderabad.bits-pilani.ac.in>
2022-01-03 19:30:20 +0530

    Code review by sanket

--- lib/views/screens/events/eventDescription.dart
+++ lib/views/screens/events/eventDescription.dart
@@ -31,203 +31,205 @@ class _EventDescriptionState extends State<EventDescription> {
           ),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-        child: Stack(
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  widget.eventModel.title,
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 28,
-                    color: Colors.black,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+          child: Stack(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    widget.eventModel.title,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 28,
+                      color: Colors.black,
+                    ),
                   ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          children: [
-                            Icon(Icons.place),
-                            SizedBox(width: 5.0),
-                            Text(
-                              widget.eventModel.location,
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 18,
-                                color: Colors.black,
-                              ),
-                            ),
-                          ],
-                        ),
-                        SizedBox(height: 30.0),
-                        Row(
-                          crossAxisAlignment: CrossAxisAlignment.baseline,
-                          textBaseline: TextBaseline.alphabetic,
-                          children: [
-                            Text(
-                              "Organised by :",
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 18,
-                                color: AppTheme.secondaryColor,
-                              ),
-                            ),
-                            SizedBox(height: 30.0),
-                            Text(
-                              widget.eventModel.organiser,
-                              overflow: TextOverflow.ellipsis,
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 18,
-                                color: Colors.black,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-                SizedBox(height: 30.0),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          children: [
-                            Icon(Icons.calendar_today),
-                            SizedBox(width: 5.0),
-                            Text(
-                              widget.eventModel.date.toString(),
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 18,
-                                  color: Colors.black),
-                            ),
-                          ],
-                        ),
-                        Row(
-                          children: [
-                            Icon(Icons.alarm),
-                            SizedBox(width: 5.0),
-                            Text(
-                              //TODO Uhh I should do a better job of rounding off times instead of removing the seconds part with a regexp
-                              "${widget.eventModel.startTime.replaceFirst(RegExp(r":\d+$"), "")} - ${widget.eventModel.endTime.replaceFirst(RegExp(r":\d+$"), "")}",
-                              style: const TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 18,
-                                  color: Colors.black),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                    Container(
-                      height: 50.0,
-                      width: 120.0,
-                      decoration: BoxDecoration(
-                        color: AppTheme.secondaryColor,
-                        borderRadius: BorderRadius.circular(12.0),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Icon(
-                              Icons.people,
-                              color: Colors.white,
-                            ),
-                            Text(
-                              widget.eventModel.users.length.toString(),
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 18.0,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-                SizedBox(height: 30.0),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Description :',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18,
-                          color: Colors.black),
-                    ),
-                    Text(
-                      widget.eventModel.description,
-                      style: TextStyle(
-                          fontWeight: FontWeight.w400,
-                          fontSize: 18,
-                          color: Colors.black),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 50.0),
-              ],
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                SizedBox(),
-                if (false)
-                  InkWell(
-                    onTap: () {
-                      //   Navigator.push(
-                      //       context,
-                      //       MaterialPageRoute(
-                      //           builder: (context) => GiveFeedBackScreen()));
-                    },
-                    child: Padding(
-                      padding: const EdgeInsets.all(80.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          // Container(
-                          //   decoration: BoxDecoration(
-                          //     color: AppTheme.secondaryColor,
-                          //     borderRadius: BorderRadius.all(Radius.circular(15)),
-                          //   ),
-
-                          //   child: Padding(
-                          //     padding: const EdgeInsets.all(12.0),
-                          //     child: Text(
-                          //       "Give FeedBack",
-                          //       style: TextStyle(
-                          //         fontWeight: FontWeight.bold,
-                          //         fontSize: 24,
-                          //         color: Colors.white,
-                          //       ),
-                          //     ),
-                          //   ),
-                          // ),
+                          Row(
+                            children: [
+                              Icon(Icons.place),
+                              SizedBox(width: 5.0),
+                              Text(
+                                widget.eventModel.location,
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18,
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 30.0),
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.baseline,
+                            textBaseline: TextBaseline.alphabetic,
+                            children: [
+                              Text(
+                                "Organised by :",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18,
+                                  color: AppTheme.secondaryColor,
+                                ),
+                              ),
+                              SizedBox(height: 30.0),
+                              Text(
+                                widget.eventModel.organiser,
+                                overflow: TextOverflow.ellipsis,
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18,
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ],
+                          ),
                         ],
                       ),
-                    ),
+                    ],
                   ),
-              ],
-            ),
-          ],
+                  SizedBox(height: 30.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              Icon(Icons.calendar_today),
+                              SizedBox(width: 5.0),
+                              Text(
+                                widget.eventModel.date.toString(),
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18,
+                                    color: Colors.black),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Icon(Icons.alarm),
+                              SizedBox(width: 5.0),
+                              Text(
+                                //TODO Uhh I should do a better job of rounding off times instead of removing the seconds part with a regexp
+                                "${widget.eventModel.startTime.replaceFirst(RegExp(r":\d+$"), "")} - ${widget.eventModel.endTime.replaceFirst(RegExp(r":\d+$"), "")}",
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18,
+                                    color: Colors.black),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      Container(
+                        height: 50.0,
+                        width: 120.0,
+                        decoration: BoxDecoration(
+                          color: AppTheme.secondaryColor,
+                          borderRadius: BorderRadius.circular(12.0),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Icon(
+                                Icons.people,
+                                color: Colors.white,
+                              ),
+                              Text(
+                                widget.eventModel.users.length.toString(),
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(height: 30.0),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Description :',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                            color: Colors.black),
+                      ),
+                      Text(
+                        widget.eventModel.description,
+                        style: TextStyle(
+                            fontWeight: FontWeight.w400,
+                            fontSize: 18,
+                            color: Colors.black),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 50.0),
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(),
+                  if (false)
+                    InkWell(
+                      onTap: () {
+                        //   Navigator.push(
+                        //       context,
+                        //       MaterialPageRoute(
+                        //           builder: (context) => GiveFeedBackScreen()));
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.all(80.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            // Container(
+                            //   decoration: BoxDecoration(
+                            //     color: AppTheme.secondaryColor,
+                            //     borderRadius: BorderRadius.all(Radius.circular(15)),
+                            //   ),
+
+                            //   child: Padding(
+                            //     padding: const EdgeInsets.all(12.0),
+                            //     child: Text(
+                            //       "Give FeedBack",
+                            //       style: TextStyle(
+                            //         fontWeight: FontWeight.bold,
+                            //         fontSize: 24,
+                            //         color: Colors.white,
+                            //       ),
+                            //     ),
+                            //   ),
+                            // ),
+                          ],
+                        ),
+                      ),
+                    ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/views/screens/more/volunteerMoreInfo.dart
+++ lib/views/screens/more/volunteerMoreInfo.dart
@@ -20,129 +20,131 @@ class _VolunteerMoreInfoScreenState extends State<VolunteerMoreInfoScreen> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: Padding(
-          padding: AppTheme.screenPadding,
-          child: Column(
-            children: [
-              SizedBox(height: 80.0),
-              Column(
-                children: [
-                  Center(
-                    child: Hero(
-                      tag: "logo",
-                      child: Container(
-                        height: 150,
-                        width: 150,
-                        child: Image(
-                          image: AssetImage('assets/images/NSS-symbol.png'),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: AppTheme.screenPadding,
+            child: Column(
+              children: [
+                SizedBox(height: 80.0),
+                Column(
+                  children: [
+                    Center(
+                      child: Hero(
+                        tag: "logo",
+                        child: Container(
+                          height: 150,
+                          width: 150,
+                          child: Image(
+                            image: AssetImage('assets/images/NSS-symbol.png'),
+                          ),
                         ),
                       ),
                     ),
+                    SizedBox(height: 20.0),
+                    Text(
+                      'NOT ME BUT YOU!',
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 80.0),
+                InkWell(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => VolunteerAboutUsScreen()));
+                  },
+                  child: Container(
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'About Us',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 18),
+                          ),
+                          SizedBox(
+                            width: 160,
+                          ),
+                          Icon(
+                            Icons.chevron_right_rounded,
+                            color: Colors.black,
+                            size: 30.0,
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
-                  SizedBox(height: 20.0),
-                  Text(
-                    'NOT ME BUT YOU!',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                  ),
-                ],
-              ),
-              SizedBox(height: 80.0),
-              InkWell(
-                onTap: () {
-                  Navigator.push(
+                ),
+                SizedBox(height: 20.0),
+                InkWell(
+                  onTap: () {
+                    Navigator.push(
                       context,
                       MaterialPageRoute(
-                          builder: (context) => VolunteerAboutUsScreen()));
-                },
-                child: Container(
-                  child: Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          'About Us',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 18),
-                        ),
-                        SizedBox(
-                          width: 160,
-                        ),
-                        Icon(
-                          Icons.chevron_right_rounded,
-                          color: Colors.black,
-                          size: 30.0,
-                        ),
-                      ],
+                        builder: (context) => VolunteerContactUs(),
+                      ),
+                    );
+                  },
+                  child: Container(
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Contact Us',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 18),
+                          ),
+                          Icon(
+                            Icons.chevron_right_rounded,
+                            color: Colors.black,
+                            size: 30.0,
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(height: 20.0),
-              InkWell(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => VolunteerContactUs(),
-                    ),
-                  );
-                },
-                child: Container(
-                  child: Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          'Contact Us',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 18),
-                        ),
-                        Icon(
-                          Icons.chevron_right_rounded,
-                          color: Colors.black,
-                          size: 30.0,
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(height: 20.0),
-              InkWell(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => VolunteerDropSuggestionsScreen(),
-                    ),
-                  );
-                },
-                child: Container(
-                  child: Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          'Drop Suggestions',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 18),
-                        ),
-                        Icon(
-                          Icons.chevron_right_rounded,
-                          color: Colors.black,
-                          size: 30.0,
-                        ),
-                      ],
+                SizedBox(height: 20.0),
+                InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => VolunteerDropSuggestionsScreen(),
+                      ),
+                    );
+                  },
+                  child: Container(
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Drop Suggestions',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 18),
+                          ),
+                          Icon(
+                            Icons.chevron_right_rounded,
+                            color: Colors.black,
+                            size: 30.0,
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(height: 20.0),
-              LogOutButton(),
-            ],
+                SizedBox(height: 20.0),
+                LogOutButton(),
+              ],
+            ),
           ),
         ),
       ),
